1d057bd0060432bd54be280a8af52031dd036fed
keerti <keerti@gsbitlabs.com.au Logout>
2021-01-06 10:33:13 +0530

    error_1

--- configurations/db.loader.js
+++ configurations/db.loader.js
@@ -1,6 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const config = require('./config');
 
-mongoose.connect('mongodb://localhost:27017/Tuser',{useCreateIndex:true,useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true})
+
+const mongoURI = 'mongodb://' + config.db.user + ':' + config.db.password + '@' + config.db.host + '/' + config.db.database
+const UrlDB = 'mongodb://admin:'+encodeURIComponent('gsbitlabs@123')+'@65.0.105.97/practice'
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose.connect(mongoURI,{useCreateIndex:true,useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true})
+
+// mongoose.connect('mongodb://localhost:27017/Tuser',{useCreateIndex:true,useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(UrlDB,{useCreateIndex:true,useFindAndModify:true,useNewUrlParser:true,useUnifiedTopology:true})
 mongoose.connection.on('connected',()=>{
     console.log('DB conected');
 })
